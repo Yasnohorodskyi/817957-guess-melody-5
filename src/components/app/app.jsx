7d6d9a4fba28004ac3ec5a758d455fd655c1583a
Yasnohorodskyi <62336521+Yasnohorodskyi@users.noreply.github.com>
2020-10-04 17:39:55 +0300

    4. Учебный проект: начинаем программировать

---
+++ src/components/app/app.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import WelcomeScreen from "../welcome-screen/welcome-screen";
+import PropTypes from "prop-types";
+
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorsCount} = props;
+
+  return (
+    <WelcomeScreen errorsCount={errorsCount} />
+  );
+};
+
+WelcomeScreen.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
+
+export default App;
